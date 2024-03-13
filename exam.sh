e42007cc8c61adda9a41fb52d2d63da27f924311
薛重阳 <22373243@buaa.edu.cn>
2024-03-13 11:35:40 +0000

    Update file exam.sh

---
+++ exam.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+mkdir ./test
+cp ./code ./test
+cat ./code/14.c
+for ((i=0; i<=15; i++))
+do
+  gcc -c ./test/code/i.c
+done
+gcc ./test/code/*.o -o ./test/hello
+touch ./test/err.txt
+./test/hello >&2 ./test/err.txt
+mv ./test/err.txt ./
+chmod 655 ./err.txt
+n1=1
+n2=1
+if [ $# -eq 1 ]; then n1=$($1); fi
+if [ $# -eq 2 ]; then n1=$($1); n2=$($2) fi
+n=$($(n1)+$(n2))
+sed -n '($n)p' err.txt >&2
+
