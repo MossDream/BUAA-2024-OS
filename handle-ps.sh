1664269f9cb43d069aa33a188268a763cb568e65
薛重阳 <22373243@buaa.edu.cn>
2024-03-13 13:28:55 +0000

    Update file handle-ps.sh

--- handle-ps.sh
+++ handle-ps.sh
@@ -39,13 +39,13 @@ elif [ ! -z "$CMD" ]; then
     # Your code here. (2/3)
     grep $CMD $FILE
 elif [ ! -z $PID ]; then
-    # Your code here. (3/3)
-    # p=$PID
-    # while [ $p -ne 0 ]
-    # do
-    #     awk -v pid=p '$2==pid {print $3;p=$3}'
-    # done
-    awk -v pid=p '$2=pid {print $3}'
+    Your code here. (3/3)
+    p=$PID
+    while [ $p -ne 0 ]
+    do
+        awk -v pid=p '$2==pid {print $3}'
+        awk '{p=$3}'
+    done
 else
     usage
     exit 1
