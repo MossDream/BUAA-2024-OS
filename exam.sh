fb3dffa985ea03e8ec0855f97b86fd1178c9e419
薛重阳 <22373243@buaa.edu.cn>
2024-03-13 11:54:37 +0000

    Update file exam.sh

--- exam.sh
+++ exam.sh
@@ -10,9 +10,10 @@ done
 cd ..
 gcc ./code/*.o -o hello
 touch err.txt
-./hello >&2 ./test/err.txt
+./hello >&2 ./err.txt
 mv ./err.txt ../
-chmod 655 ./err.txt
+cd ..
+chmod 655 err.txt
 n1=1
 n2=1
 if [ $# -eq 1 ]; then n1=$($1); fi
