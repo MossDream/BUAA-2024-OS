337f4daca0f0e898d3cf00af327791a3d533f7ef
MossDream <124897852+MossDream@users.noreply.github.com>
2024-03-16 22:43:59 +0800

    lab1-1

--- tools/readelf/readelf.c
+++ tools/readelf/readelf.c
@@ -10,11 +10,12 @@
  * Post-Condition:
  *   Returns 0 if 'binary' isn't an ELF, otherwise returns 1.
  */
-int is_elf_format(const void *binary, size_t size) {
+int is_elf_format(const void *binary, size_t size)
+{
 	Elf32_Ehdr *ehdr = (Elf32_Ehdr *)binary;
 	return size >= sizeof(Elf32_Ehdr) && ehdr->e_ident[EI_MAG0] == ELFMAG0 &&
-	       ehdr->e_ident[EI_MAG1] == ELFMAG1 && ehdr->e_ident[EI_MAG2] == ELFMAG2 &&
-	       ehdr->e_ident[EI_MAG3] == ELFMAG3;
+		   ehdr->e_ident[EI_MAG1] == ELFMAG1 && ehdr->e_ident[EI_MAG2] == ELFMAG2 &&
+		   ehdr->e_ident[EI_MAG3] == ELFMAG3;
 }
 
 /* Overview:
@@ -28,11 +29,13 @@ int is_elf_format(const void *binary, size_t size) {
  *   If success, output the address of every section in ELF.
  */
 
-int readelf(const void *binary, size_t size) {
+int readelf(const void *binary, size_t size)
+{
 	Elf32_Ehdr *ehdr = (Elf32_Ehdr *)binary;
 
 	// Check whether `binary` is a ELF file.
-	if (!is_elf_format(binary, size)) {
+	if (!is_elf_format(binary, size))
+	{
 		fputs("not an elf file\n", stderr);
 		return -1;
 	}
@@ -43,14 +46,19 @@ int readelf(const void *binary, size_t size) {
 	Elf32_Half sh_entry_count;
 	Elf32_Half sh_entry_size;
 	/* Exercise 1.1: Your code here. (1/2) */
+	sh_table = binary + ehdr->e_shoff;
+	sh_entry_count = ehdr->e_shnum;
+	sh_entry_size = ehdr->e_shentsize;
 
 	// For each section header, output its index and the section address.
 	// The index should start from 0.
-	for (int i = 0; i < sh_entry_count; i++) {
+	for (int i = 0; i < sh_entry_count; i++)
+	{
 		const Elf32_Shdr *shdr;
 		unsigned int addr;
 		/* Exercise 1.1: Your code here. (2/2) */
-
+		shdr = (Elf32_Shdr *)(sh_table + i * sh_entry_size);
+		addr = shdr->sh_addr;
 		printf("%d:0x%x\n", i, addr);
 	}
 
