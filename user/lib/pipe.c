cb869f9368cb29ce14aa4d2c8f63a7f04b101e13
MossDream <124897852+MossDream@users.noreply.github.com>
2024-05-23 22:10:44 +0800

    lab6-2

--- user/lib/pipe.c
+++ user/lib/pipe.c
@@ -170,7 +170,7 @@ static int pipe_read(struct Fd *fd, void *vbuf, u_int n, u_int offset)
 		p->p_rpos++;
 	}
 
-	user_panic("pipe_read not implemented");
+	// user_panic("pipe_read not implemented");
 }
 
 /* Overview:
@@ -218,7 +218,7 @@ static int pipe_write(struct Fd *fd, const void *vbuf, u_int n, u_int offset)
 		p->p_wpos++;
 	}
 
-	user_panic("pipe_write not implemented");
+	// user_panic("pipe_write not implemented");
 
 	return n;
 }
