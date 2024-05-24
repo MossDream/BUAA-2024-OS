3602286d6ad10807d52102df6a8cdc199ad3777e
MossDream <124897852+MossDream@users.noreply.github.com>
2024-05-24 16:40:59 +0800

    lab6-c-1:modified:   user/lib/spawn.c

--- user/lib/spawn.c
+++ user/lib/spawn.c
@@ -123,10 +123,27 @@ int spawn(char *prog, char **argv)
 {
 	// Step 1: Open the file 'prog' (the path of the program).
 	// Return the error if 'open' fails.
+	// Lab6 chanllenge 1: 如果是因为文件没有以.b结尾而打开失败，那么添加.b结尾再尝试打开
 	int fd;
 	if ((fd = open(prog, O_RDONLY)) < 0)
 	{
-		return fd;
+		int length = strlen(prog);
+		if (length < 2 || prog[length - 2] != '.' || prog[length - 1] != 'b')
+		{
+			char *new_prog = (char *)malloc(length + 3);
+			strcpy(new_prog, prog);
+			new_prog[length] = '.';
+			new_prog[length + 1] = 'b';
+			new_prog[length + 2] = '\0';
+			if ((fd = open(new_prog, O_RDONLY)) < 0)
+			{
+				return fd;
+			}
+		}
+		else
+		{
+			return fd;
+		}
 	}
 
 	// Step 2: Read the ELF header (of type 'Elf32_Ehdr') from the file into 'elfbuf' using
